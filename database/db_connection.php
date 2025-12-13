4d014fd2053101926519d1c53194f8c2aeddad59
jlrandle2 <123025705+jlrandle2@users.noreply.github.com>
2025-12-13 02:00:45 -0500

    Add files via upload

---
+++ database/db_connection.php
@@ -0,0 +1,14 @@
+<?php
+$servername = "localhost";
+$username = "jrandle4";
+$password = "jrandle4"; 
+$dbname = "jrandle4";
+
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
